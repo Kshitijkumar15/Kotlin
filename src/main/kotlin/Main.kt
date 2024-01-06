6b9ad716773dc70544446e75f62bdf57949dee54
Kshitij kumar singh <kshitijkumarsingh58@gmail.com>
2024-01-06 15:13:22 +0530

    Initial Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,27 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main(){
+//    println("Hello World")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+//    var myVariable:Int =15
+//    println(myVariable)
+
+//    var myVariable:Int =15
+//    println("The value of my variable is $myVariable")
+
+//    var myVariable:Int =15
+//    myVariable=7
+//    println("The value of my variable is $myVariable")
+
+//    val myVariable:Int =15
+//    println("In Val values can't be overwritten $myVariable")
+
+//    var myVariable:Boolean=true
+//    println("The value of my variable is  $myVariable")
+//    myVariable=false
+//    println("The value of my variable is now  $myVariable")
+
+//    var myVariable:String="Kshitij"
+//    println("Hi, I am $myVariable")
+//    var myVariableName: Int=21
+//    println("and i am  $myVariable years old")
+
 }
